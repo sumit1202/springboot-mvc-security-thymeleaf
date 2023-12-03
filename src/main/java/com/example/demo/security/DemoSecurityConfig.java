07fba9eb19b941390b5fccb9f9b01a74c72c4466
sumitkumar <sumitkumar1202@gmail.com>
2023-12-03 15:13:35 +0530

    user-details-password-bcrypt-jdbc-mysql

--- src/main/java/com/example/demo/security/DemoSecurityConfig.java
+++ src/main/java/com/example/demo/security/DemoSecurityConfig.java
@@ -19,29 +19,6 @@ public class DemoSecurityConfig {
                 return new JdbcUserDetailsManager(dataSource);
         }
 
-        // @Bean
-        // public InMemoryUserDetailsManager userDetailsManager() {
-        // UserDetails john = User.builder()
-        // .username("john")
-        // .password("{noop}test123")
-        // .roles("EMPLOYEE")
-        // .build();
-
-        // UserDetails mary = User.builder()
-        // .username("mary")
-        // .password("{noop}test123")
-        // .roles("EMPLOYEE", "MANAGER")
-        // .build();
-
-        // UserDetails susan = User.builder()
-        // .username("susan")
-        // .password("{noop}test123")
-        // .roles("EMPLOYEE", "MANAGER", "ADMIN")
-        // .build();
-
-        // return new InMemoryUserDetailsManager(john, mary, susan);
-        // }
-
         @Bean
         public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
                 httpSecurity.authorizeHttpRequests(
